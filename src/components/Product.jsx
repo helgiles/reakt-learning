2f7e486f5c9cb194717c92b95ef3f6e4715264db
Олег Слюсар <eternityonly1@gmail.com>
2024-02-05 20:43:04 +0200

    + photo

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,18 @@
 // src/Product.jsx
 
 export const Product = () => {
+  const price = 999;
   return (
     <div>
       <h2>Tacos</h2>
       <img
-        src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
+        src="../../public/pexels-photo.jpeg"
         alt="Tacos With Lime"
         width="640"
       />
-      <p>Price: 999 credits</p>
+      <p>Price: {price}credits</p>
     </div>
   );
 };
+
+// <https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>
