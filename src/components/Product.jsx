1d13a2428bbcb6f9230610d60a883f455b7da866
Олег Слюсар <eternityonly1@gmail.com>
2024-02-06 18:01:19 +0200

    + зображення за замовчуванням

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,28 +1,15 @@
 // src/Product.jsx
 
-export const Product = props => {
+export const Product = ({
+  name,
+  imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+  price,
+}) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img src={props.imgUrl} alt={props.name} width="480" />
-      <p>Price: {props.price} credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
     </div>
   );
 };
-
-// export const Product = () => {
-//   const price = 999;
-//   return (
-//     <div>
-//       <h2>Tacos</h2>
-//       <img
-//         src="../../public/pexels-photo.jpeg"
-//         alt="Tacos With Lime"
-//         width="640"
-//       />
-//       <p>Price: {price}credits</p>
-//     </div>
-//   );
-// };
-
-// <https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>
